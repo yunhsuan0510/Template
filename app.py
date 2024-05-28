4b81ad82bff933acc7e6c39e645761da6541cbc8
yunhsuan0510 <166582621+yunhsuan0510@users.noreply.github.com>
2024-05-28 11:40:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,26 @@
-from flask import Flask, request
 from linebot import LineBotApi, WebhookHandler
-from linebot.models import PostbackAction, URIAction, MessageAction, TemplateSendMessage, ButtonsTemplate
-import os
-
-app = Flask(__name__)
-
-# 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-
-@app.route('/push_message')
-def push_message():
-    line_bot_api.push_message(os.getenv('USER_ID'), TemplateSendMessage(
-        alt_text='ButtonsTemplate',
-        template=ButtonsTemplate(
-            thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo.jpg',
-            title='OXXO.STUDIO',
-            text='這是按鈕樣板',
-            actions=[
-                PostbackAction(
-                    label='postback',
-                    data='發送 postback'
-                ),
-                MessageAction(
-                    label='說 hello',
-                    text='hello'
-                ),
-                URIAction(
-                    label='前往 STEAM 教育學習網',
-                    uri='https://steam.oxxostudio.tw'
-                )
-            ]
-        )
-    ))
-    return 'Message sent!'
-
-@app.route('/callback', methods=['POST'])
-def callback():
-    # 在這裡處理 Line Bot 的回調請求
-    # 例如，解析請求內容，處理訊息等等
-    return 'OK'
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+# 需要額外載入對應的函示庫
+from linebot.models import PostbackAction,URIAction, MessageAction, TemplateSendMessage, ButtonsTemplate
+line_bot_api = LineBotApi('te/bzWwRmL8YVwG2nJdEUaaen2qBMvUDjvMxiWxylV3B5zt9Tto+N4IVKxMAB9uUG6yq4981CsitJ6IDTkFegFs8mmaYCw7bhFDgMA+2BJkqP3rrQKYTU1meE4QfcbyNgWbsYiY07EsYnh+YgXdMaQdB04t89/1O/w1cDnyilFU=')
+line_bot_api.push_message('Ueb0d6dea2a95c12fdf716b078d624834', TemplateSendMessage(
+    alt_text='ButtonsTemplate',
+    template=ButtonsTemplate(
+        thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo.jpg',
+        title='OXXO.STUDIO',
+        text='這是按鈕樣板',
+        actions=[
+            PostbackAction(
+                label='postback',
+                data='發送 postback'
+            ),
+            MessageAction(
+                label='說 hello',
+                text='hello'
+            ),
+            URIAction(
+                label='前往 STEAM 教育學習網',
+                uri='https://steam.oxxostudio.tw'
+            )
+        ]
+    )
+))
