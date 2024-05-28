17f4a075d16a3dcb8d0a91fb573ebe7a0a71eb02
yunhsuan0510 <166582621+yunhsuan0510@users.noreply.github.com>
2024-05-28 11:26:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,40 @@
-from linebot import LineBotApi, WebhookHandler
-# 需要額外載入對應的函示庫
-from linebot.models import PostbackAction,URIAction, MessageAction, TemplateSendMessage, ButtonsTemplate
+from flask import Flask
+from linebot import LineBotApi
+from linebot.models import PostbackAction, URIAction, MessageAction, TemplateSendMessage, ButtonsTemplate
 import os
+
+
+app = Flask(__name__)
+
 # 必須放上自己的Channel Access Token
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 
-line_bot_api.push_message(os.getenv('USER_ID'), TemplateSendMessage(
-    alt_text='ButtonsTemplate',
-    template=ButtonsTemplate(
-        thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo.jpg',
-        title='OXXO.STUDIO',
-        text='這是按鈕樣板',
-        actions=[
-            PostbackAction(
-                label='postback',
-                data='發送 postback'
-            ),
-            MessageAction(
-                label='說 hello',
-                text='hello'
-            ),
-            URIAction(
-                label='前往 STEAM 教育學習網',
-                uri='https://steam.oxxostudio.tw'
-            )
-        ]
-    )
-))
+@app.route('/push_message')
+def push_message():
+    line_bot_api.push_message(os.getenv('USER_ID'), TemplateSendMessage(
+        alt_text='ButtonsTemplate',
+        template=ButtonsTemplate(
+            thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo.jpg',
+            title='OXXO.STUDIO',
+            text='這是按鈕樣板',
+            actions=[
+                PostbackAction(
+                    label='postback',
+                    data='發送 postback'
+                ),
+                MessageAction(
+                    label='說 hello',
+                    text='hello'
+                ),
+                URIAction(
+                    label='前往 STEAM 教育學習網',
+                    uri='https://steam.oxxostudio.tw'
+                )
+            ]
+        )
+    ))
+    return 'Message sent!'
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
