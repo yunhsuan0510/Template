253064dffc048ee747dddfdd2059c7b7290af143
yunhsuan0510 <166582621+yunhsuan0510@users.noreply.github.com>
2024-05-28 10:47:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from linebot import LineBotApi, WebhookHandler
 # 需要額外載入對應的函示庫
 from linebot.models import PostbackAction,URIAction, MessageAction, TemplateSendMessage, ButtonsTemplate
-line_bot_api = LineBotApi('你的 Channel access token')
-line_bot_api.push_message('你的 user ID', TemplateSendMessage(
+
+# 必須放上自己的Channel Access Token
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+
+line_bot_api.push_message(os.getenv('USER_ID'), TemplateSendMessage(
     alt_text='ButtonsTemplate',
     template=ButtonsTemplate(
         thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo.jpg',
