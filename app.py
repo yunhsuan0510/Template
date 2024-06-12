155dcd1ab9688f85335a2b1268c976016fdf0d52
yunhsuan0510 <166582621+yunhsuan0510@users.noreply.github.com>
2024-06-12 22:09:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-import openai, os
-
+import openai
+import os
 from flask import Flask, request
-
-# 載入 LINE Message API 相關函式庫
 from linebot import LineBotApi, WebhookHandler
-from linebot.models import TextSendMessage   # 載入 TextSendMessage 模組
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import TextSendMessage
 import json
 
 app = Flask(__name__)
+
+# Initialize LineBotApi and WebhookHandler
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
 @app.route("/", methods=['POST'])
 def linebot():
@@ -15,33 +18,36 @@ def linebot():
     json_data = json.loads(body)
     print(json_data)
     try:
-        line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-        handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
         signature = request.headers['X-Line-Signature']
         handler.handle(body, signature)
+
         tk = json_data['events'][0]['replyToken']
         msg = json_data['events'][0]['message']['text']
-        # 取出文字的前五個字元，轉換成小寫
+
+        # Extract the first five characters and convert to lowercase
         ai_msg = msg[:6].lower()
         reply_msg = ''
-        # 取出文字的前五個字元是 hi ai:
+
         if ai_msg == 'hi ai:':
             openai.api_key = os.getenv("OPENAI_API_KEY")
-            # 將第六個字元之後的訊息發送給 OpenAI
+            # Send the message to OpenAI
             response = openai.Completion.create(
                 model='text-davinci-003',
                 prompt=msg[6:],
                 max_tokens=256,
                 temperature=0.5,
-                )
-            # 接收到回覆訊息後，移除換行符號
-            reply_msg = response["choices"][0]["text"].replace('\n','')
+            )
+            # Get the response text and remove newline characters
+            reply_msg = response["choices"][0]["text"].replace('\n', '')
         else:
             reply_msg = msg
+
         text_message = TextSendMessage(text=reply_msg)
-        line_bot_api.reply_message(tk,text_message)
-    except:
-        print('error')
+        line_bot_api.reply_message(tk, text_message)
+    except InvalidSignatureError:
+        print('Invalid signature. Please check your channel access token/channel secret.')
+    except Exception as e:
+        print(f'Error: {e}')
     return 'OK'
 
 if __name__ == "__main__":
